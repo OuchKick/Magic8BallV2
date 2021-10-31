6e74c9fbdfcbc737744f0d7b355c79d2ac91435a
Adam Reed <thirdeyeadam@gmail.com>
2021-10-31 11:59:05 -0500

    deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
 import NavbarTop from './components/Navbar/Navbar';
 import History from './components/History/History';
 import MagicEightBall from './components/MagicBall/MagicBall';
@@ -9,7 +8,6 @@ import MagicEightBall from './components/MagicBall/MagicBall';
 
 function App() {
   return (
-    <HashRouter basename="/">
     <div className="backgroundGradient">
     <Router>
       <NavbarTop />
@@ -23,7 +21,6 @@ function App() {
       </Switch>
     </Router>
     </div>
-    </HashRouter>
   );
 }
 
