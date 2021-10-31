ca61c1e54eed45f7e4df472a1e726b9889b46a78
Adam Reed <thirdeyeadam@gmail.com>
2021-10-31 12:24:13 -0500

    deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
 import NavbarTop from './components/Navbar/Navbar';
 import History from './components/History/History';
 import MagicEightBall from './components/MagicBall/MagicBall';
